66ecf542aac1eb129e6041a97fa0036a2e8c96a4
chunguslol <agustin.ledezma@ucb.edu.bo>
2024-12-12 09:43:49 -0400

    Añadir url del front

--- src/app.js
+++ src/app.js
@@ -10,7 +10,13 @@ app.set("port", process.env.PORT || 4000);
 
 // Middlewares
 // const corsOptions = {origin: "http://localhost:4200"}
-app.use(cors());
+const cors = require("cors");
+const corsOptions = {
+  origin: ["https://graceful-mooncake-ead7ad.netlify.app/"], // Cambia por la URL de tu frontend.
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsOptions));
+
 app.use(morgan("dev"));
 app.use(express.json());
 
